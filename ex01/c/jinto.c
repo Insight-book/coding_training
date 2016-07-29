37da35fe853ef8f7623c101c0f737eaefe1460f0
jaypark <jaypark@gmail.com>
2016-07-29 23:32:07 +0900

    gets to scanf

--- ex01/c/jinto.c
+++ ex01/c/jinto.c
@@ -1,12 +1,23 @@
 // need gcc, tested on macosx
 // gcc jinto.c;./a.out
+// note : buffer overflow
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
   char name[256];
+  char greeting[256];
+  char *pos;
+
   printf("What is your name? ");
-  gets (name);
-  printf ("Hello, %s, nice to meet you!\n", name);
+  scanf( "%s", name ); 
+
+  strcpy(greeting, "Hello, ");
+  strcat(greeting, name);
+  strcat(greeting, ", nice to meet you!\n");
+
+  puts(greeting);
+
   return 0;
 }
